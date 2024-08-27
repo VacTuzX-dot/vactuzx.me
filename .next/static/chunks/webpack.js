dbc566512f72b4a2221d66f9c3abe194b6f914ba
VacTuz <taweesaknumma@gmail.com>
2024-08-27 16:00:07 +0700

    dasdzx

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "17eab3397a950fe2"; }
+/******/ 		__webpack_require__.h = function() { return "3803477ddd5d53f7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/api/v1/repos.jsx
+++ src/pages/api/v1/repos.jsx
@@ -1,24 +1,24 @@
-import "dotenv/config"; // Ensure environment variables are loaded before using them
 import axios from "axios";
 import config from "../../../../config";
 
 export default async function handler(req, res) {
+  var token = config.github.token1;
+
+  const { data } = await axios.get(
+    `https://api.github.com/users/vactuzx-dot/repos`,
+    {
+      headers: {
+        Authorization: `token ${token}`,
+      },
+    }
+  );
+
   try {
-    const token = config.github.token1;
-    const { data } = await axios.get(
-      `https://api.github.com/users/vactuzx-dot/repos`,
-      {
-        headers: {
-          Authorization: `token ${token}`,
-        },
-      }
-    );
     return res.json({
       status: true,
       data: data,
     });
   } catch (err) {
-    console.error("Error fetching GitHub repos:", err);
     return res.json({
       status: false,
       data: err.message,
